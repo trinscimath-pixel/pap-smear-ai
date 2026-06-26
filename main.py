f5211ecbc16004a6bb30ad861b34dc95b5ec05fc
trinscimath-pixel <trinscimath@gmail.com>
2026-06-26 11:41:59 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -2,16 +2,18 @@ from fastapi import FastAPI, UploadFile, File
 from PIL import Image
 import io
 import torch
-import timm
+from torchvision import models
+import torch.nn as nn
 from torchvision import transforms
 app = FastAPI(title="CerviVision AI API")
 # ==========================
 # Model
 # ==========================
-model = timm.create_model(
-    "vit_base_patch16_224",
-    pretrained=False,
-    num_classes=5
+model = models.mobilenet_v3_large(weights=None)
+
+model.classifier[3] = nn.Linear(
+    model.classifier[3].in_features,
+    5
 )
 model.load_state_dict(
     torch.load("model.pth", map_location="cpu")
@@ -21,10 +23,10 @@ model.eval()
 # Classes
 # ==========================
 class_names = [
-    "NILM",
     "ASC-US",
-    "LSIL",
     "HSIL",
+    "LSIL",
+    "NILM",
     "SCC"
 ]
 # ==========================
@@ -47,21 +49,27 @@ def home():
 # ==========================
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
-image_bytes = await file.read()
+    image_bytes = await file.read()
+
     image = Image.open(
         io.BytesIO(image_bytes)
     ).convert("RGB")
+
     x = transform(image).unsqueeze(0)
+
     with torch.no_grad():
         outputs = model(x)
         probs = torch.softmax(outputs, dim=1)[0]
+
     result = {
         class_names[i]: float(probs[i])
         for i in range(5)
     }
+
     prediction = class_names[
         torch.argmax(probs).item()
     ]
+
     return {
         "prediction": prediction,
         "probabilities": result
